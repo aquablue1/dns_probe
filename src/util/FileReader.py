aa8f468e96abebd23b48bfc7d41ea5c256191e0b
weisser <94apieceofcake@gmail.com>
2018-08-09 11:06:35 -0600

    add batchFileReader

--- src/util/FileReader.py
+++ src/util/FileReader.py
@@ -12,11 +12,11 @@ import os
 class fileReader():
     def __init__(self, filename):
         self.filename = filename
-        self.hasNext = True
+        # self.hasNext = True
 
     def __iter__(self):
         try:
-            self.file = open(self.filename)
+            self.file = open(self.filename, 'r')
         except FileNotFoundError:
             print("Filename %d not found in file system." % self.filename)
         return self
@@ -31,16 +31,53 @@ class fileReader():
             nextLine = self.file.readline()
         if nextLine == "":
             # print("Reach the end of current line - %s" % self.filename)
-            self.hasNext = False
+            # self.hasNext = False
             raise StopIteration
         else:
             return nextLine.strip()
 
 
+class batchFileReader():
+    def __init__(self, foldername, cookie=""):
+        self.cookie = cookie
+        self.foldername = foldername
+        self.selectedFileList = []
+        self.curFile = None
+
+    def __iter__(self):
+        try:
+            for filename in os.listdir(self.foldername):
+                if self.cookie in filename:
+                    self.selectedFileList.append(self.foldername+"/"+filename)
+        except OSError:
+            print("Folder %s does not exists." % self.foldername)
+        self.curFile = open(self.selectedFileList[0], 'r')
+        self.selectedFileList.pop(0)
+        return self
+
+    def __next__(self):
+        nextLine = self.curFile.readline()
+        while nextLine == "":
+            if self.selectedFileList == []:
+                # All contents are read, exit
+                raise StopIteration
+            else:
+                # one of the file has been finished, check if
+                self.curFile = open(self.selectedFileList[0], 'r')
+                self.selectedFileList.pop(0)
+                nextLine = self.curFile.readline()
+        while nextLine != "" and nextLine[0] == "#":
+            nextLine = self.curFile.readline()
+        return nextLine.strip()
+
+
 if __name__ == '__main__':
     filename = "../../data/Conn_WeeklySummary_2018-03.log"
-    newFile = fileReader(filename)
+    # newFile = fileReader(filename)
 
-    for line in newFile:
-        # line_list = line.split("\t")
+
+    foldername = "../../data/"
+    cookie = "sample"
+    batchFile = batchFileReader(foldername, cookie)
+    for line in batchFile:
         print(line)
